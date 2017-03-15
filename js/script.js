9cdaf6e73f4c01a8571bfc43849273ecf1243a6d
Nana Ofosu Budu <nanaofosubudu@gmail.com>
2017-03-14 20:06:06 -0500

    Loaded to group

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-    
-    $('.tablet ul').hide("slide", { direction: "left" }, 1000);
-
+    $('.tablet ul').hide("slide", {
+        direction: "left"
+    }, 1000);
     $("#burger").click(function () {
-
-       // $('.tablet ul').animate({width:'toggle'},350);
+        // $('.tablet ul').animate({width:'toggle'},350);
         $('.tablet ul').stop().slideToggle('200');
-        $("#special").fadeToggle( "fast", "linear" );
+        $(".main").fadeToggle("fast", "linear");
+        $("footer").fadeToggle("fast", "linear");
+        $("#special").fadeToggle("fast", "linear");
     });
 });
